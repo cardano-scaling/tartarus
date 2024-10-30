3949b015a0b3dab0537518d4a8a3212553dc0bb2
Arnaud Bailly <arnaud.bailly@iohk.io>
2024-10-30 13:44:50 +0100

    Use 'standard' date switches instead of GNU ones

--- proserpina/configure.sh
+++ proserpina/configure.sh
@@ -8,9 +8,9 @@ CONFIG="${ROOT}/config"
 podman network create --ignore --disable-dns --subnet "$(cat "${ROOT}"/CIDR-a)" proserpina-a
 podman network create --ignore --disable-dns --subnet "$(cat "${ROOT}"/CIDR-b)" proserpina-b
 
-START_TIME="$(date -d "now - 1 seconds" +%s)"
+START_TIME="$(date -v -1S +%s)"
 sed -i -e 's/"startTime": [0-9]*/"startTime": '"${START_TIME}/" "${CONFIG}/byron/genesis.json"
-sed -i -e 's/"systemStart":"[^"]*"/"systemStart":"'"$(date -d @"${START_TIME}" -u +%FT%T)"'Z"/' "${CONFIG}/shelley-genesis.json"
+sed -i -e 's/"systemStart":"[^"]*"/"systemStart":"'"$(date -j -f "%s" "${START_TIME}" -u +%FT%T)"'Z"/' "${CONFIG}/shelley-genesis.json"
 
 mkdir -p nodes/{1,2,3,4,5,6,7,8,9}
 rm -rf nodes/?/{db,socket}
